9ef4470b4a10f5d58eb8f27516f6d08302f49233
Jai ghai <63393416+jai-ghai@users.noreply.github.com>
2023-12-27 17:14:37 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
 
-
-export const connectDB = async()=>{
-   const {connection} = await mongoose.connect(process.env.MONGO_URI);
-   console.log(`MongoDB connected with ${connection.host}`);
-}
+export const connectDB = async () => {
+  try {
+    const { connection } = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected with ${connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    // Handle the error, throw an exception, or exit the application
+  }
+};
